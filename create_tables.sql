e41aa2da289ed908bef696f9d75d41bd20caf774
jackie <jackie.woodburn@gmail.com>
2021-03-28 20:28:45 +0100

    incorporated and updated wk 1 scripts using Victoria's comments from last week. made one table for people rather than having author, employee, reader.

--- create_tables.sql
+++ create_tables.sql
@@ -3,10 +3,18 @@
 
 use library;
 
-create table authors (
-author_id int auto_increment not null primary key,
+create table roles (
+role_id int auto_increment not null primary key,
+role varchar(50) not null
+);
+
+create table person (
+person_id int auto_increment not null primary key,
 first_name varchar(50) not null,
-last_name varchar(50) not null
+last_name varchar(50) not null,
+age int,
+role_id int not null,
+foreign key (role_id) references roles (role_id)
 );
 
 create table genres (
@@ -18,25 +26,25 @@ create table books (
 book_id int auto_increment not null primary key,
 title varchar(100) not null,
 author_id int not null,
-isbn int not null,
+isbn char(16),
 genre_id int,
 shelf_reference char(6) not null,
-foreign key (author_id) references authors (author_id),
+foreign key (author_id) references person (person_id),
 foreign key (genre_id) references genres (genre_id)
 );
 
-create table readers (
-reader_id int auto_increment not null primary key,
-first_name varchar(50) not null,
-last_name varchar(50) not null,
-age int not null
-);
+-- create table readers (
+-- reader_id int auto_increment not null primary key,
+-- first_name varchar(50) not null,
+-- last_name varchar(50) not null,
+-- age int not null
+-- );
 
-create table employees (
-employee_id int auto_increment not null primary key,
-first_name varchar(50) not null,
-last_name varchar(50) not null
-);
+-- create table employees (
+-- employee_id int auto_increment not null primary key,
+-- first_name varchar(50) not null,
+-- last_name varchar(50) not null
+-- );
 
 create table checkouts(
 checkout_id int auto_increment not null primary key,
@@ -49,7 +57,7 @@ date_return date not null
 
 alter table checkouts 
 add foreign key (book_id) references books (book_id),
-add foreign key (reader_id) references readers (reader_id); 
+add foreign key (reader_id) references person (person_id); 
 
 -- alter table books
 -- alter column isbn char(16);
--- fill_tables_sample_data.sql
+++ fill_tables_sample_data.sql
@@ -1,5 +1,10 @@
 use library;
 
+insert into roles (role)
+values ('Author'),
+('Employee'),
+('Reader');
+
 insert into genres (genre)
 values ('Fantasy'),
 ('Sci-Fi'),
@@ -9,39 +14,37 @@ values ('Fantasy'),
 select *
 from genres;
 
-insert into readers (first_name, last_name, age)
-values ('Kiera', 'Knightley', 39),
-('Orlando', 'Bloom', '45'),
-('Johnny', 'Depp', 50),
-('Geoffrey', 'Rush', '55');
-
-insert into readers (first_name, last_name, age)
-values ('Bill', 'Nighy', 70);
+insert into person (first_name, last_name, age, role_id)
+values ('Kiera', 'Knightley', 39, 3),
+('Orlando', 'Bloom', '45', 3),
+('Johnny', 'Depp', 50, 3),
+('Geoffrey', 'Rush', '55', 3);
 
 -- select *
 -- from readers;
 
-INSERT INTO employees (first_name, last_name)
-values ('Rachel', 'Green'),
-('Ross', 'Geller'),
-('Chandler', 'Bing'),
-('Monica', 'Geller'),
-('Phoebe', 'Buffay');
+INSERT INTO person (first_name, last_name, role_id)
+values ('Rachel', 'Green', 2),
+('Ross', 'Geller', 2),
+('Chandler', 'Bing', 2),
+('Monica', 'Geller', 2),
+('Phoebe', 'Buffay', 2),
+('Ryan', 'Holiday', 1),
+('Robert', 'Greene', 1),
+('Napoleon', 'Hill', 1);
 
--- select * from employees;
-
-insert into authors (first_name, last_name)
-values ('Ryan', 'Holiday'),
-('Robert', 'Greene'),
-('Napoleon', 'Hill');
+select *
+from person;
 
 insert into books (title, author_id, isbn, shelf_reference)
-values ('The Daily Stoic', 1, 37683878, 'KD12C2'),
-('The 48 Laws of Power', 2, 27683878, 'EK49D1'),
-('The Law of Success', 3, 17683978, 'QX47E3'),
-('Think and Grow Rich', 3, 37691878, 'ZJ14F9'),
-('Think and Grow Rich', 3, 37691878, 'ZJ14F9');
--- Should we have added in the titles as a foreign key?
+values ('The Daily Stoic', 10, '3768387878019876', 'KD12C2'),
+('The 48 Laws of Power', 11, '3768187878019876', 'EK49D1'),
+('The Law of Success', 12, '3768356778019876', 'QX47E3'),
+('Think and Grow Rich', 12, '2768387878019876', 'ZJ14F9'),
+('Think and Grow Rich', 12, '2768387878019876', 'ZJ14F9');
+
+select *
+from books;
 
 insert into checkouts (book_id, date_out, date_due, reader_id, date_return)
 values (1, '2021-01-29', '2021-02-12', 2, '2021-02-10');
@@ -49,18 +52,9 @@ values (1, '2021-01-29', '2021-02-12', 2, '2021-02-10');
 alter table checkouts
 modify date_return date;
 
-
-
 insert into checkouts (book_id, date_out, date_due, reader_id)
 values (2, '2021-03-05', '2021-03-19', 5);
 -- How do we alter a column that is not null to be null
 
-
-
-
-
-
-
-
-
-
+select *
+from checkouts;
